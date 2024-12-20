0b3a2cb2f44e5415f35eb3fd446cfe2a649d444f
ithodol <ddmdjesz1@gmail.com>
2024-12-20 18:37:07 +0900

    join tos 다음버튼 수정

--- js/join_tos.js
+++ js/join_tos.js
@@ -1,9 +1,9 @@
-let customerList = [
-    {customerCode : 1, id : "test1", password : "1234", name : "유재석", birth : "831205", phone : "010-1111-1111", address : "인천 부평구", clause : true},
-    {customerCode : 2, id : "test2", password : "4567", name : "강호동", birth : "800506", phone : "010-2222-2222", address : "인천 계양구", clause : false},
-    {customerCode : 3, id : "test3", password : "7890", name : "신동엽", birth : "791125", phone : "010-3333-3333", address : "서울 종로구", clause : true},
-    {customerCode : 4, id : "test4", password : "1234", name : "홍길동", birth : "920505", phone : "010-4444-4444", address : "부산 해운대구", clause : false},
- ];
+// let customerList = [
+//     {customerCode : 1, id : "test1", password : "1234", name : "유재석", birth : "831205", phone : "010-1111-1111", address : "인천 부평구", clause : true},
+//     {customerCode : 2, id : "test2", password : "4567", name : "강호동", birth : "800506", phone : "010-2222-2222", address : "인천 계양구", clause : false},
+//     {customerCode : 3, id : "test3", password : "7890", name : "신동엽", birth : "791125", phone : "010-3333-3333", address : "서울 종로구", clause : true},
+//     {customerCode : 4, id : "test4", password : "1234", name : "홍길동", birth : "920505", phone : "010-4444-4444", address : "부산 해운대구", clause : false},
+//  ];
 
 
 // 약관동의 함수
@@ -47,6 +47,7 @@ function tos(){
 
     if(contBtn1 == true && contBtn3 == true){
         console.log('동의');
+        location.href = "./join_info.html";
     }else if(contBtn2 == false && contBtn4 == false){
         alert('약관에 동의해주시기 바랍니다');
         return;
