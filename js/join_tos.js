3d3863b6c5fd2f62bbf43730e3edff9af856c5a4
ithodol <ddmdjesz1@gmail.com>
2024-12-21 00:56:47 +0900

    체크박스 수정 실패

--- js/join_tos.js
+++ js/join_tos.js
@@ -7,28 +7,108 @@
 
 
 // 약관동의 함수
-function tos(){
+/*
+    if(check1==false||chaeck2==false){
+    }else if(check1==true||chaeck2==true){
+        전체체크박스=true
+    }
+*/
+function allCheck(){
+
+    let all = document.querySelector('.all')
     let contBtn1 = document.querySelector('.yn1yes')
     let contBtn2 = document.querySelector('.yn1no')
     let contBtn3 = document.querySelector('.yn2yes')
     let contBtn4 = document.querySelector('.yn2no')
 
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+/*
+function tos(){
+    let checkTrue = []
+    let checkFalse = []
+    let contBtn1 = document.querySelector('.yn1yes').value
+    let contBtn2 = document.querySelector('.yn1no').value
+    let contBtn3 = document.querySelector('.yn2yes').value
+    let contBtn4 = document.querySelector('.yn2no').value
+
     //console.log(contBtn1);
     //console.log(contBtn2);
 
+    checkTrue.push(contBtn1,contBtn3);
+    checkFalse.push(contBtn2,contBtn4);
+    //console.log(checkBottom);
+    
+    
+    let joinState = false;
 
-    if(contBtn1.checked){
-        contBtn1 = true;
+    if(checkTrue == "" || checkTrue == "true"){
+        console.log('성공')
+    }else if(checkFalse == "" || checkFalse == "false"){
+        console.log('실패')
     }
-    if(contBtn2.checked){
-        contBtn2 = false;
+
+    if(check1==false||chaeck2==false){
+        }else if(check1==true||chaeck2==true){
+            전체체크박스=true
+        }
+    
+
+    
+    for(let index = 0; index <= checkTrue.length-1; index++){
+        if(checkFalse == "" || checkTrue[index] == "true"){
+            
+            joinState = true;
+        }
     }
-    if(contBtn3.checked){
-        contBtn3 = true;
+
+    for(let index = 0; index <= checkFalse.length-1; index++){
+
     }
-    if(contBtn4.checked){
-        contBtn4 = false;
+    
+
+
+
+
+
+    /*
+    // 상태 체크 
+    if(joinState){
+        console.log('확인');
+        //location.href = "./join_info.html";
+    }else{
+        alert('약관에 동의해주시기 바랍니다.');
     }
+    
+
+
+    // let next = ''
+    // if(contBtn1.checked){
+    //     contBtn1 = true;
+    //     next = true;
+    // }
+    // if(contBtn2.checked){
+    //     contBtn2 = false;
+    // }
+    // if(contBtn3.checked){
+    //     contBtn3 = true;
+    //     next = true;
+    // }
+    // if(contBtn4.checked){
+    //     contBtn4 = false;
+    // }
 
     /*
     console.log("-----------------");
@@ -37,56 +117,52 @@ function tos(){
     console.log(contBtn3);
     console.log(contBtn4);
     console.log("-----------------");
-    */
-
-    // 아무것도 체크하지 않으면 리턴
-    if(contBtn1 == null || contBtn2 == null || contBtn3 == null || contBtn4 == null){
-        alert('약관에 동의해주시기 바랍니다');
-        return;
-    }
-
-    if(contBtn1 == true && contBtn3 == true){
-        console.log('동의');
-        location.href = "./join_info.html";
-    }else if(contBtn2 == false && contBtn4 == false){
-        alert('약관에 동의해주시기 바랍니다');
-        return;
-    }
-
-
-
-    if(contBtn1 == true && contBtn4 == false){
-        alert('약관에 동의해주시기 바랍니다');
-        return;
-    }
-    if(contBtn2 == false && contBtn3 == true){
-        alert('약관에 동의해주시기 바랍니다');
-        return;
-    }
     
-  
+
+    // 1번,2번 체크하지 않음
+    // if(contBtn1 == null || contBtn2 == null || contBtn3 == null || contBtn4 == null){
+    //     alert('약관에 동의해주시기 바랍니다');
+    //     return;
+    // }
+
+    // 1번 동의함, 2번 동의함
+    // if(contBtn1 == true && contBtn3 == true){
+    //     location.href = "./join_info.html";
+    // }
+    // if(contBtn2 == false && contBtn4 == false){ // 1번 동의하지 않음, 2번 동의하지 않음
+    //     alert('약관에 동의해주시기 바랍니다');
+    //     return;
+    // }
 
 
+    // // 1번 동의함, 2번 동의하지 않음
+    // if(contBtn1 == true && contBtn4 == false){
+    //     alert('약관에 동의해주시기 바랍니다');
+    //     return;
+    // }
+    // // 1번 동의하지 않음, 2번 동의함
+    // if(contBtn2 == false && contBtn3 == true){
+    //     alert('약관에 동의해주시기 바랍니다');
+    //     return;
+    // }
+    
+    // if(next){
+    //     location.href = "./join_info.html";
+    // }else{
+    //     alert('약관에 동의해주시기 바랍니다')
+    //     return;
+    // }
 }
+*/
+
+
+
+
 
 
 function popup(){
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
