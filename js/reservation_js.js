f4c7c9beb46c3be4bcc02d4c34eb5736b9e528f3
Ryuyena0305 <Ryuyena0305@gmail.com>
2024-12-20 18:17:43 +0900

    pj1

--- js/reservation_js.js
+++ js/reservation_js.js
@@ -1,43 +1,3 @@
-let customerList = [
-    { customerCode: 1, id: "test1", password: "1234", name: "유재석", birth: "831205", phone: "010-1111-1111", address: "인천 부평구", clause: true },
-    { customerCode: 2, id: "test2", password: "4567", name: "강호동", birth: "800506", phone: "010-2222-2222", address: "인천 계양구", clause: false },
-    { customerCode: 3, id: "test3", password: "7890", name: "신동엽", birth: "791125", phone: "010-3333-3333", address: "서울 종로구", clause: true },
-    { customerCode: 4, id: "test4", password: "1234", name: "홍길동", birth: "920505", phone: "010-4444-4444", address: "부산 해운대구", clause: false },
-];
-
-let bookingList = [
-    {bookingCode : 1, customerCode : 1, roomCode : "A-1", headCount : 0, checkIn : "2024-02-14", checkOut : "2024-02-16", reviewState : true},
-    {bookingCode : 2, customerCode : 2, roomCode : "A-5", headCount : 0, checkIn : "2024-03-15", checkOut : "2024-03-20", reviewState : true},
-    {bookingCode : 3, customerCode : 1, roomCode : "A-2", headCount : 0, checkIn : "2024-03-17", checkOut : "2024-03-18", reviewState : false},
-    {bookingCode : 4, customerCode : 2, roomCode : "B-1", headCount : 0, checkIn : "2024-03-20", checkOut : "2024-03-22", reviewState : false},
-    {bookingCode : 5, customerCode : 3, roomCode : "A-7", headCount : 0, checkIn : "2024-03-25", checkOut : "2024-03-28", reviewState : false},
-    {bookingCode : 6, customerCode : 4, roomCode : "C-4", headCount : 0, checkIn : "2024-03-27", checkOut : "2024-04-01", reviewState : true},
-    {bookingCode : 7, customerCode : 2, roomCode : "B-3", headCount : 0, checkIn : "2024-05-17", checkOut : "2024-05-20", reviewState : true},
-    {bookingCode : 8, customerCode : 1, roomCode : "C-1", headCount : 0, checkIn : "2024-06-06", checkOut : "2024-06-08", reviewState : false},
-    {bookingCode : 9, customerCode : 4, roomCode : "B-1", headCount : 0, checkIn : "2024-06-06", checkOut : "2024-06-07", reviewState : true},
-    {bookingCode : 10, customerCode : 3, roomCode : "B-3", headCount : 0, checkIn : "2024-10-10", checkOut : "2024-10-25", reviewState : false},
-    {bookingCode : 11, customerCode : 3, roomCode : "C-2", headCount : 0, checkIn : "2024-12-01", checkOut : "2024-12-05", reviewState : true},
-    {bookingCode : 12, customerCode : 2, roomCode : "B-4", headCount : 0, checkIn : "2025-02-05", checkOut : "2025-02-08", reviewState : false}
-]
-
-
-let roomList = [
-	{roomCode : "A-1", roomName : "스탠다드 싱글", roomPrice : 210000, headCount : 1, roomNumber : 101},
-	{roomCode : "A-2", roomName : "스탠다드 싱글", roomPrice : 210000, headCount : 1, roomNumber : 102},
-	{roomCode : "A-3", roomName : "스탠다드 싱글", roomPrice : 210000, headCount : 1, roomNumber : 103},
-	{roomCode : "A-4", roomName : "스탠다드 더블", roomPrice : 231000, headCount : 2, roomNumber : 104},
-	{roomCode : "A-5", roomName : "스탠다드 더블", roomPrice : 231000, headCount : 2, roomNumber : 105},
-	{roomCode : "A-6", roomName : "스탠다드 트윈", roomPrice : 231000, headCount : 2, roomNumber : 106},
-	{roomCode : "A-7", roomName : "스탠다드 트리플", roomPrice : 286000, headCount : 3, roomNumber : 107},
-	{roomCode : "B-1", roomName : "디럭스 더블", roomPrice : 275000, headCount : 2, roomNumber : 201},
-	{roomCode : "B-2", roomName : "디럭스 더블", roomPrice : 275000, headCount : 2, roomNumber : 202},
-	{roomCode : "B-3", roomName : "디럭스 트리플", roomPrice : 330000, headCount : 3, roomNumber : 203},
-	{roomCode : "B-4", roomName : "디럭스 트리플", roomPrice : 330000, headCount : 3, roomNumber : 204},
-	{roomCode : "C-1", roomName : "주니어 스위트", roomPrice : 385000, headCount : 2, roomNumber : 301},
-	{roomCode : "C-2", roomName : "프리미어 스위트", roomPrice : 473000, headCount : 2, roomNumber : 302},
-	{roomCode : "C-3", roomName : "이그제큐티브 스위트", roomPrice : 572000, headCount : 2, roomNumber : 303},
-	{roomCode : "C-4", roomName : "아미드 스위트", roomPrice : 660000, headCount : 2, roomNumber : 304},
-];
 
 
 /*객실예약 옆 roomlist 출력 */
@@ -181,7 +141,8 @@ function infoInput(){
         let infoBookingCode = bookingList[index].bookingCode 
     }
     infos = [
-        {bookingCode : infoBookingCode, customerCode : 1, roomCode : check_roomcode, headCount : number_con2, checkIn : checkin_time, checkOut :  checkout_time, reviewState : false}]
+        {bookingCode : infoBookingCode, customerCode : 1, roomCode : check_roomcode, headCount : number_con2, checkIn : checkin_time, checkOut :  checkout_time, reviewState : false}
+    ]
 
     let room = 회원정보목록반환함수();
 
@@ -191,11 +152,6 @@ function infoInput(){
     return;
 
 }
-/**
- * 
- * bookingList = [
-	{bookingCode : 1, customerCode : 1, roomCode : "A-1", checkIn : "2024-02-14", checkOut : "2024-02-16"},
- */
 
 function 회원정보목록반환함수(){ 
     let memberList = sessionStorage.getItem( 'bookingList' );
